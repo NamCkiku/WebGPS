d6e7650f359906e01d42b6927cf65adac843c106
Tran Hoang Nam (PPM) <namth2@bagroup.vn>
2019-10-31 08:45:17 +0700

    dasdasd

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,7 +5,7 @@ import { Routes,RouterModule } from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 
 const routes: Routes = [
-
+ 
   { path: '', component: LoginComponent }
 
 ];
